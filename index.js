f91b6ae67390cdb8ae287be0efb1c0a84793f7ad
Rujul Pawar <pawarrujul27@gmail.com>
2025-02-16 18:55:54 +0530

    Started Working on Env Mon

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const WebSocket = require('ws');
+const mqtt = require('mqtt');
 
 const app = express();
 const PORT = 8000;
@@ -38,6 +40,47 @@ app.post('/login', (req, res) => {
 // Route to render nagarnetra.ejs
 app.get('/nagarnetra', (req, res) => {
     return res.render('nagarnetra');
+});
+
+app.get('/environmental-monitoring', (req, res) => {
+    return res.render('environmental-monitoring');
+});
+
+// MQTT
+const options = {
+    host: process.env.MQTT_HOST,
+    port: parseInt(process.env.MQTT_PORT),
+    protocol: 'mqtts',
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD
+};
+
+const client = mqtt.connect(options);
+
+// Initialize the WebSocket server
+const wss = new WebSocket.Server({ port: 8080 });
+
+// MQTT setup
+client.on('connect', function () {
+    console.log('Connected to MQTT broker');
+    client.subscribe('sensor/temperature');
+    client.subscribe('sensor/pressure');
+    client.subscribe('sensor/humidity');
+    client.subscribe('sensor/latitude');
+    client.subscribe('sensor/longitude');
+});
+
+client.on('message', function (topic, message) {
+    // Broadcast the message to all connected WebSocket clients
+    wss.clients.forEach((ws) => {
+        if (ws.readyState === WebSocket.OPEN) {
+            ws.send(JSON.stringify({ topic, value: message.toString() }));
+        }
+    });
+});
+
+client.on('error', function (error) {
+    console.error('MQTT Error:', error);
 });
 
 // Route for the maintenance page
